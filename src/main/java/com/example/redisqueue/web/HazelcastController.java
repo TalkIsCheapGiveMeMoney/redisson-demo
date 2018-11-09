c4ec8ce07caad84f06cf835a424168d3a342a19c
zhushangjin <381599113@qq.com>
2018-11-09 10:02:23 +0800

    hazelcast 分布式内存

--- src/main/java/com/example/redisqueue/web/HazelcastController.java
+++ src/main/java/com/example/redisqueue/web/HazelcastController.java
@@ -1,12 +1,16 @@
 package com.example.redisqueue.web;
 
+import com.example.redisqueue.entity.Trunk;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IQueue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BlockingQueue;
 
 /**
  * Author:ZhuShangJin
@@ -39,6 +43,7 @@ public class HazelcastController {
     @GetMapping(value = "/readAll")
     public Map<String, String> readAllDataFromHazelcast() {
         Map<String, String> hazelcastMap = hazelcastInstance.getMap("my-map");
+        BlockingQueue<Trunk> ee = hazelcastInstance.getQueue("11");
         return hazelcastInstance.getMap("my-map");
     }
 
